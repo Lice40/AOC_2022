2d96f45b98d95c0bda1ce2afed1b1e40c1193966
Alice <munch.alice31@gmail.com>
2022-12-24 16:37:02 +0100

    AOC_2022_D2_P1

---
+++ AOC_2022_2/main.cpp
@@ -0,0 +1,70 @@
+
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+#define PLAYER_ROCK 1
+#define PLAYER_PAPER 2
+#define PLAYER_SCISOR 3
+
+#define PLAYER_WIN 6
+#define PLAYER_DRAW 3
+#define PLAYER_LOSE 0
+
+
+int main(){
+    fstream file;
+    file.open("inputs.txt", ios::in);
+    int sum=0;
+    if(!file.is_open()){
+        cout<<"erreur d'ouverture du fichier"<<endl;
+        exit(-1);
+    }
+    string line;
+    while(getline(file, line)){
+       switch(line[0]){
+        case 'A': //l'ennemi joue pierre
+            switch(line[2]){
+                case 'X': //on joue pierre
+                    sum = sum + PLAYER_DRAW + PLAYER_ROCK; 
+                break;
+                case 'Y': //on joue papier
+                    sum = sum + PLAYER_WIN + PLAYER_PAPER;
+                break;
+                case 'Z': //on joue ciseaux
+                    sum = sum + PLAYER_LOSE + PLAYER_SCISOR;
+                break;
+            }
+        break;
+        case 'B':// l'ennemi joue papier
+            switch(line[2]){
+                case 'X': //on joue pierre
+                    sum = sum + PLAYER_LOSE + PLAYER_ROCK; 
+                break;
+                case 'Y': //on joue papier
+                    sum = sum + PLAYER_DRAW + PLAYER_PAPER; 
+                break;
+                case 'Z': //on joue ciseaux
+                    sum = sum + PLAYER_WIN + PLAYER_SCISOR;
+                break;
+            }
+        break;
+        case 'C':// l'ennemi joue ciseaux
+            switch(line[2]){
+                case 'X': //on joue pierre
+                    sum = sum + PLAYER_WIN+PLAYER_ROCK; 
+                break;
+                case 'Y': //on joue papier
+                    sum = sum + PLAYER_LOSE + PLAYER_PAPER;
+                break;
+                case 'Z': //on joue ciseaux
+                    sum = sum + PLAYER_DRAW + PLAYER_SCISOR;
+                break;
+            }
+        break;
+        
+       }
+    }
+    cout<<"points totaux: "<<sum<<endl;
+    return 0;
+}
