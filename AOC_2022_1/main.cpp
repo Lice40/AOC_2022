9d69c772a91be27838435a2d5bffec1f1eac269f
Alice <munch.alice31@gmail.com>
2022-12-24 15:08:09 +0100

    day 1_P1 + debut day1_P2

---
+++ AOC_2022_1/main.cpp
@@ -0,0 +1,44 @@
+
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+void update(int* table[], int val){
+    cout<<"valeur à maj: "<<val<< endl;
+    cout<<"table = { "<< (*table)[0]<<" , "<< (*table)[1]<<" , "<<(*table)[2]<<" }"<<endl;
+    for(int i = 0; i < 3; i ++){
+        if(val > (*table)[i]){
+            (*table)[i] = val;
+            cout<<"table = { "<< (*table)[0]<<" , "<< (*table)[1]<<" , "<<(*table)[2]<<" }"<<endl;
+            return;
+        }
+    }
+    cout<<"table = { "<< (*table)[0]<<" , "<< (*table)[1]<<" , "<<(*table)[2]<<" }"<<endl;
+    return;
+}
+
+int main(){
+    fstream file;
+    file.open("inputs.txt", ios::in);
+    int* max{new int [3] {0,0,0}};
+
+
+    int sum=0;
+    if(!file.is_open()){
+        cout<<"erreur d'ouverture du fichier"<<endl;
+        exit(-1);
+    }
+    string line;
+    while(getline(file, line)){
+        if(line == ""){
+            update(&max, sum);
+            sum = 0;
+        }else{
+            sum += stoi(line);
+        }
+    }
+    cout <<"la valeur maximale vaut donc: "<<max[0]+max[1]+max[2]<<endl;
+    delete [] max;
+    return 0;
+}
